36b5370275260eeae6b1038d649aa54885973a98
竜也 秋山 <fallmountain624@outlook.jp>
2023-07-10 22:41:04 +0900

    会員制度の仕様変更

--- src/chapter1/Test5_main.java
+++ src/chapter1/Test5_main.java
@@ -5,17 +5,17 @@ import chapter1.Test5_card.Card;
 public class Test5_main {
 
 	public static void main(String[] args) {
-		Card card = Card.SPADES;
+		Card card = Card.DIAMONDS;
 		
 		switch(card) {
 			case SPADES:
 			case CLUBS:
-			System.out.println("Black");
+			System.out.println("Gold");
 			break;
 			
 			case DIAMONDS:
 			case HEARTS:
-			System.out.println("Red");
+			System.out.println("Platinum");
 			break;
 			
 		}
